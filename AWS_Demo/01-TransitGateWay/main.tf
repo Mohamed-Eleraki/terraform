5c1dcc62ea63c6b35204bfec4b898d3fea59b328
Mohamed El Eraky <93321621+Mohamed-Eleraki@users.noreply.github.com>
2024-01-29 10:54:54 +0200

    Update main.tf

---
+++ AWS_Demo/01-TransitGateWay/main.tf
@@ -0,0 +1,46 @@
+# Configure aws provider
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+}
+
+# Configure aws provider
+provider "aws" {
+    region = "us-east-1"
+}
+
+# Variable
+variable "dev_vpc1_cider_block" {
+    description = "Development step cider block"
+}
+
+variable "dev_vpc2_cider_block" {}
+
+
+# Create a VPC-1
+resource "aws_vpc" "vpc-1" {
+    cider_block = var.dev_vpc1_cider_block
+    tags = {
+        Name = "${var.environment}-vpc-1"
+    }
+}
+
+# Create a VPC-2
+resource "aws_vpc" "vpc-2" {
+    cider_block = var.dev_vpc2_cider_block
+    tags = {
+        Name = "${var.environment}-vpc-1"
+    }
+}
+
+# Create a VPC-3
+resource "aws_vpc" "vpc-3" {
+    cider_block = var.dev_vpc3_cider_block
+    tags = {
+        Name = "${var.environment}-vpc-3"
+    }
+}
