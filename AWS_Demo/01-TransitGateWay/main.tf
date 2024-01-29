76b08fb30e92520fb9ba90379c413262b5841b63
Mohamed El Eraky <93321621+Mohamed-Eleraki@users.noreply.github.com>
2024-01-29 12:27:44 +0200

    replace cider_block with cidr_block

--- AWS_Demo/01-TransitGateWay/main.tf
+++ AWS_Demo/01-TransitGateWay/main.tf
@@ -14,17 +14,17 @@ provider "aws" {
 }
 
 # Variable
-variable "dev_vpc1_cider_block" {
+variable "dev_vpc1_cidr_block" {
     description = "Development step cider block"
 }
 
-variable "dev_vpc2_cider_block" {}
-variable "dev_vpc3_cider_block" {}
+variable "dev_vpc2_cidr_block" {}
+variable "dev_vpc3_cidr_block" {}
 variable "environment" {}
 
 # Create a VPC-1
 resource "aws_vpc" "vpc-1" {
-    cider_block = var.dev_vpc1_cider_block
+    cidr_block = var.dev_vpc1_cidr_block
     tags = {
         Name = "${var.environment}-vpc-1"
     }
@@ -32,7 +32,7 @@ resource "aws_vpc" "vpc-1" {
 
 # Create a VPC-2
 resource "aws_vpc" "vpc-2" {
-    cider_block = var.dev_vpc2_cider_block
+    cidr_block = var.dev_vpc2_cidr_block
     tags = {
         Name = "${var.environment}-vpc-1"
     }
@@ -40,7 +40,7 @@ resource "aws_vpc" "vpc-2" {
 
 # Create a VPC-3
 resource "aws_vpc" "vpc-3" {
-    cider_block = var.dev_vpc3_cider_block
+    cidr_block = var.dev_vpc3_cidr_block
     tags = {
         Name = "${var.environment}-vpc-3"
     }
