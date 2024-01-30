db52882e59b75f71edbd9eadbf5fe4643c7a276d
Mohamed El Eraky <93321621+Mohamed-Eleraki@users.noreply.github.com>
2024-01-30 07:38:03 +0200

    Create the full Diagram successfully

--- AWS_Demo/01-TransitGateWay/main.tf
+++ AWS_Demo/01-TransitGateWay/main.tf
@@ -41,7 +41,7 @@ variable "dev_subnet3_cidr_block" {
 }
 
 
-# Create a VPC-1
+# Create a VPC-1 | VPC-1 Configurations
 resource "aws_vpc" "vpc-1" {
     cidr_block = var.dev_vpc1_cidr_block
     tags = {
@@ -67,20 +67,57 @@ resource "aws_subnet" "subnet-1" {
   }
 }
 
+# Create a route table
+resource "aws_route_table" "rt-1" {
+  vpc_id = aws_vpc.vpc-1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"  # Represents all IP addresses (i.e., internet-bound traffic)
+    gateway_id = aws_internet_gateway.igw-1.id
+  }
+
+  route {
+    cidr_block = "13.0.1.0/24"  # Represents all IP addresses (i.e., internet-bound traffic)
+    transit_gateway_id = aws_ec2_transit_gateway.tgw-1.id
+  }
+
+  route {
+    cidr_block = "14.0.1.0/24"  # Represents all IP addresses (i.e., internet-bound traffic)
+    transit_gateway_id = aws_ec2_transit_gateway.tgw-1.id
+  }
+
+
+
+  tags = {
+    Name = "${var.environment}-rt-1"
+  }
+}
+
+# configure route table association
+resource "aws_route_table_association" "rt-ass-1" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.rt-1.id
+}
+
+
 # Create ec2-1 in vpc-1
 resource "aws_instance" "ec2-1" {
   ami = "ami-0a3c3a20c09d6f377"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id = aws_subnet.subnet-1.id
   tags = {
     Name = "${var.environment}-ec2-1"
   }
 
 }
 
-# Create a VPC-2
+
+# Create a VPC-2 | VPC-2 Configurations
 resource "aws_vpc" "vpc-2" {
     cidr_block = var.dev_vpc2_cidr_block
     tags = {
-        Name = "${var.environment}-vpc-1"
+        Name = "${var.environment}-vpc-2"
     }
 }
 
@@ -94,7 +131,7 @@ resource "aws_internet_gateway" "igw-2" {
 
 # Create subnet-2 for VPC-2
 resource "aws_subnet" "subnet-2" {
-  vpc_id = aws_vpc.vpc-1.id
+  vpc_id = aws_vpc.vpc-2.id
   cidr_block = var.dev_subnet2_cidr_block
   availability_zone = "us-east-1e"
   tags = {
@@ -102,16 +139,50 @@ resource "aws_subnet" "subnet-2" {
   }
 }
 
+# Create a route table
+resource "aws_route_table" "rt-2" {
+  vpc_id = aws_vpc.vpc-2.id
+
+  route {
+    cidr_block = "0.0.0.0/0"  # Represents all IP addresses (i.e., internet-bound traffic)
+    gateway_id = aws_internet_gateway.igw-2.id
+  }
+  
+  route {
+    cidr_block = "12.0.1.0/24"  # Represents all IP addresses (i.e., internet-bound traffic)
+    transit_gateway_id = aws_ec2_transit_gateway.tgw-1.id
+  }
+
+  route {
+    cidr_block = "14.0.1.0/24"  # Represents all IP addresses (i.e., internet-bound traffic)
+    transit_gateway_id = aws_ec2_transit_gateway.tgw-1.id
+  }
+  tags = {
+    Name = "${var.environment}-rt-2"
+  }
+}
+
+# configure route table association
+resource "aws_route_table_association" "rt-ass-2" {
+  subnet_id      = aws_subnet.subnet-2.id
+  route_table_id = aws_route_table.rt-2.id
+}
+
 # Create ec2-2 in vpc-2
 resource "aws_instance" "ec2-2" {
   ami = "ami-0a3c3a20c09d6f377"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id = aws_subnet.subnet-2.id
   tags = {
     Name = "${var.environment}-ec2-2"
   }
 
 }
 
-# Create a VPC-3
+
+
+# Create a VPC-3 | Configure vpc-3
 resource "aws_vpc" "vpc-3" {
     cidr_block = var.dev_vpc3_cidr_block
     tags = {
@@ -129,7 +200,7 @@ resource "aws_internet_gateway" "igw-3" {
 
 # Create subnet-3 for VPC-3
 resource "aws_subnet" "subnet-3" {
-  vpc_id = aws_vpc.vpc-1.id
+  vpc_id = aws_vpc.vpc-3.id
   cidr_block = var.dev_subnet3_cidr_block
   availability_zone = "us-east-1c"
   tags = {
@@ -137,11 +208,89 @@ resource "aws_subnet" "subnet-3" {
   }
 }
 
+# Create a route table
+resource "aws_route_table" "rt-3" {
+  vpc_id = aws_vpc.vpc-3.id
+
+  route {
+    cidr_block = "0.0.0.0/0"  # Represents all IP addresses (i.e., internet-bound traffic)
+    gateway_id = aws_internet_gateway.igw-3.id
+  }
+
+  route {
+    cidr_block = "12.0.1.0/24"  # Represents all IP addresses (i.e., internet-bound traffic)
+    transit_gateway_id = aws_ec2_transit_gateway.tgw-1.id
+  }
+
+  route {
+    cidr_block = "13.0.1.0/24"  # Represents all IP addresses (i.e., internet-bound traffic)
+    transit_gateway_id = aws_ec2_transit_gateway.tgw-1.id
+  }
+
+
+  tags = {
+    Name = "${var.environment}-rt-3"
+  }
+}
+
+# configure route table association
+resource "aws_route_table_association" "rt-ass-3" {
+  subnet_id      = aws_subnet.subnet-3.id
+  route_table_id = aws_route_table.rt-3.id
+}
+
+
 # Create ec2-3 in vpc-3
 resource "aws_instance" "ec2-3" {
   ami = "ami-0a3c3a20c09d6f377"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id = aws_subnet.subnet-3.id
   tags = {
     Name = "${var.environment}-ec2-3"
   }
 
+}
+
+
+
+# Create Transit Gateway | Confgiure the main transitgateway
+resource "aws_ec2_transit_gateway" "tgw-1" {
+    description = "Development Main Transitgateway"
+
+    tags = {
+      Name = "${var.environment}-tgw-1"
+    }
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-att-1" {
+  subnet_ids         = [aws_subnet.subnet-1.id]
+  transit_gateway_id = aws_ec2_transit_gateway.tgw-1.id
+  vpc_id             = aws_vpc.vpc-1.id
+
+  tags = {
+    Name = "${var.environment}-tgw-att-1"
+  }
+}
+
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-att-2" {
+  subnet_ids         = [aws_subnet.subnet-2.id]
+  transit_gateway_id = aws_ec2_transit_gateway.tgw-1.id
+  vpc_id             = aws_vpc.vpc-2.id
+
+  tags = {
+    Name = "${var.environment}-tgw-att-2"
+  }
+}
+
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-att-3" {
+  subnet_ids         = [aws_subnet.subnet-3.id]
+  transit_gateway_id = aws_ec2_transit_gateway.tgw-1.id
+  vpc_id             = aws_vpc.vpc-3.id
+
+  tags = {
+    Name = "${var.environment}-tgw-att-3"
+  }
 }
