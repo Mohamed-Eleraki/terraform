09263f172a596df7ff95802a41e2a0d4f7c7076e
Mohamed El Eraky <93321621+Mohamed-Eleraki@users.noreply.github.com>
2024-02-01 11:57:35 +0200

    before change the aws_route_table

--- AWS_Demo/01-TransitGateWay/main.tf
+++ AWS_Demo/01-TransitGateWay/main.tf
@@ -341,7 +341,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_SSH-3" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4-3" {
   security_group_id = aws_security_group.secgrp-3.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  ip_protocol       = "-1" # equivalent to all ports
 }
 
 
--- AWS_Demo/03-TransitGateWay/main.tf
+++ AWS_Demo/03-TransitGateWay/main.tf
@@ -24,7 +24,8 @@ variable "environment" {}
 # VPCs, Subnet Resources
 resource "aws_vpc" "vpc-1" {
     cidr_block = "10.0.0.0/16"
-
+    enable_dns_support = true
+    enable_dns_hostnames = true
     tags = {
       Name = "${var.environment}-vpc-1"
     }
@@ -33,6 +34,7 @@ resource "aws_vpc" "vpc-1" {
 resource "aws_subnet" "subnet-1" {
   vpc_id = aws_vpc.vpc-1.id
   cidr_block = "10.0.0.0/16"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "${var.environment}-vpc-1"
@@ -40,8 +42,9 @@ resource "aws_subnet" "subnet-1" {
 }
 
 resource "aws_vpc" "vpc-2" {
-  cidr_block = "20.0.0.0/16"
-
+    cidr_block = "20.0.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
   tags = {
     Name = "${var.environment}-vpc-2"
   }
@@ -50,7 +53,7 @@ resource "aws_vpc" "vpc-2" {
 resource "aws_subnet" "subnet-2" {
     vpc_id = aws_vpc.vpc-2.id
     cidr_block = "20.0.0.0/16"
-
+    availability_zone = "us-east-1e"
     tags = {
       Name = "${var.environment}-vpc-1"
     }
@@ -83,12 +86,15 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-attachment-vpc2" {
 
 resource "aws_ec2_transit_gateway_route_table" "tgw-rt-1" {
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+  #default_propagation_route_table = true
+  #default_association_route_table = true
 }
 
 resource "aws_ec2_transit_gateway_route" "tgw-rt-vpc1" {
   destination_cidr_block = "10.0.0.0/16"
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw-rt-1.id
   transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.tgw-attachment-vpc1.id
+
 }
 
 resource "aws_ec2_transit_gateway_route" "tgw-rt-vpc2" {
@@ -105,6 +111,10 @@ resource "aws_route_table" "rt-vpc1" {
     cidr_block = "0.0.0.0/0"
     transit_gateway_id = aws_ec2_transit_gateway.tgw.id
   }
+
+  tags = {
+    Name = "${var.environment}-rt-vpc1"
+  }
 }
 
 resource "aws_route_table" "rt-vpc2" {
@@ -113,10 +123,88 @@ resource "aws_route_table" "rt-vpc2" {
     cidr_block = "0.0.0.0/0"
     transit_gateway_id = aws_ec2_transit_gateway.tgw.id
   }
+
+    tags = {
+    Name = "${var.environment}-rt-vpc2"
+  }
 }
 
 
+# Security Groups for the instances
+resource "aws_security_group" "secGrp-01" {
+  name = "allow_SSH-01"
+  vpc_id = aws_vpc.vpc-1.id
+  
+  tags = {
+    Name = "${var.environment}-secGrp-01"
+  }
+}
 
-# Create 2 security groups
-# Create 2 ec2s's
-# Test connection
+resource "aws_vpc_security_group_ingress_rule" "Allow-SSH-secGrp01" {
+  security_group_id = aws_security_group.secGrp-01.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 22
+  to_port   = 22
+  ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "Allow-outbound-secGrp01" {
+  security_group_id = aws_security_group.secGrp-01.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
+
+resource "aws_security_group" "secGrp-02" {
+  name = "allow_SSH-02"
+  vpc_id = aws_vpc.vpc-2.id
+  tags = {
+    Name = "${var.environment}-secGrp-02"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "Allow-SSH-secGrp02" {
+  security_group_id = aws_security_group.secGrp-02.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 22
+  to_port   = 22
+  ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "Allow-outbound-secGrp02" {
+  security_group_id = aws_security_group.secGrp-02.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
+
+# Create a key pair
+resource "aws_key_pair" "kp-1" {
+  key_name = "server-key"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+# ec2 Resources
+resource "aws_instance" "ec2-01" {
+  ami = "ami-0a3c3a20c09d6f377"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id = aws_subnet.subnet-1.id
+  key_name = aws_key_pair.kp-1.key_name
+  vpc_security_group_ids = [aws_security_group.secGrp-01.id]
+
+  tags = {
+    Name = "${var.environment}-ec2-01"
+  }
+}
+
+resource "aws_instance" "ec2-02" {
+  ami = "ami-0a3c3a20c09d6f377"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id = aws_subnet.subnet-2.id
+  key_name = aws_key_pair.kp-1.key_name
+  vpc_security_group_ids = [aws_security_group.secGrp-02.id]
+
+  tags = {
+    Name = "${var.environment}-ec2-02"
+  }
+}
