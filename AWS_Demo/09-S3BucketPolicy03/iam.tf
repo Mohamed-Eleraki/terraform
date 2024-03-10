0db12487c41f9d071688958b0c80c859a9722e07
Mohamed El-Eraki <mohamed.eleraki@enzagroup.global>
2024-03-10 15:39:17 +0200

    finalize 09-S3BucketPolicy03 Project

--- AWS_Demo/09-S3BucketPolicy03/iam.tf
+++ AWS_Demo/09-S3BucketPolicy03/iam.tf
@@ -1,11 +1,69 @@
-resource "aws_iam_user" "Hala" {
-    name = "Hala"
+resource "aws_iam_user" "Mohamed" {
+    name = "Mohamed"
 }
 
-resource "aws_iam_user" "Saba" {
-    name = "Saba"
+
+resource "aws_iam_user" "Taha" {  # (i.e. Consider it as external access)
+    name = "Taha"
 }
 
-resource "aws_iam_user" "Tala" {
-    name = "Tala"
+resource "aws_iam_user" "Mostafa" {
+    name = "Mostafa"
 }
+
+# Create IAM Role for Taha - (i.e. Consider it as external access)
+resource "aws_iam_role" "iam_role_get_s3_taha" {
+  name = "s3_get_access_role"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "114",
+            "Effect": "Allow",
+            "Action": "sts:AssumeRole",
+            "Principal": {
+                "AWS": "${aws_iam_user.Taha.arn}"
+            }
+        }
+    ]
+}
+EOF
+}
+
+# Create S3 get policy
+data "aws_iam_policy_document" "s3_get_access_policy_document" {
+  statement {
+    sid = "115"
+    effect = "Allow"   
+
+    actions = [ 
+      "s3:GetObject"
+     ]
+
+     resources = [ 
+      "${aws_s3_bucket.s3_01.arn}/mostafa/*"
+      ]
+  }
+}
+
+resource "aws_iam_policy" "holds_s3_get_policy" {
+  name = "holds_s3_get_policy"
+  policy = data.aws_iam_policy_document.s3_get_access_policy_document.json
+}
+
+# attche s3 get policy to the role
+resource "aws_iam_role_policy_attachment" "attach_s3_get_role" {
+  role = aws_iam_role.iam_role_get_s3_taha.name
+  policy_arn = aws_iam_policy.holds_s3_get_policy.arn
+}
+
+# # you must put the role into the user profile
+# [profile Taha]
+# region = us-east-1
+# output = json
+
+# [profile TahaAdmin]
+# role_arn = arn:aws:iam::891377122503:role/s3_get_access_role
+# source_profile = Taha
--- AWS_Demo/09-S3BucketPolicy03/s3.tf
+++ AWS_Demo/09-S3BucketPolicy03/s3.tf
@@ -29,8 +29,8 @@ resource "aws_s3_bucket" "s3_01" {
 resource "aws_s3_bucket_ownership_controls" "ownership_s3_01" {
   bucket = aws_s3_bucket.s3_01.id
   rule {
-    #object_ownership = "BucketOwnerPreferred"
-    object_ownership = "ObjectWriter"
+    object_ownership = "BucketOwnerPreferred"
+    #object_ownership = "ObjectWriter"
   }
 }
 
@@ -44,20 +44,86 @@ resource "aws_s3_bucket_acl" "acl_s3_01" {
 # Create bucket policy
 resource "aws_s3_bucket_policy" "Policy_s3_01" {
   bucket = aws_s3_bucket.s3_01.id
-  policy = <<EOD
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "111",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "${aws_iam_user.Hala.arn}"
-            },
-            "Action": "s3:PutObject",
-            "Resource": "${aws_s3_bucket.s3_01.arn}/*"
-        }
-    ]
+  policy = data.aws_iam_policy_document.policy_document_s3_01.json
 }
-  EOD
+
+# Fetch the Bucket Owner canonical id
+data "aws_canonical_user_id" "current_user" {}
+
+# Create policy document
+data "aws_iam_policy_document" "policy_document_s3_01" {
+  statement {
+    sid = "111"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [ "${aws_iam_user.Mostafa.arn}" ]
+    }    
+
+    actions = [ 
+      "s3:PutObject"
+     ]
+
+     resources = [ 
+      "${aws_s3_bucket.s3_01.arn}/mostafa/*"
+      ]
+  }
+
+  statement {
+    sid = "112"
+    effect = "Deny"
+    principals {
+      type = "AWS"
+      identifiers = [ "${aws_iam_user.Mostafa.arn}" ]
+    }    
+
+    actions = [ 
+      "s3:PutObject"
+     ]
+
+     resources = [ 
+      "${aws_s3_bucket.s3_01.arn}/mostafa/*"
+      ]
+
+      condition {
+        test = "StringNotEquals"
+        variable = "s3:x-amz-grant-full-control"
+        values = [ "id=${data.aws_canonical_user_id.current_user.id}" ]
+      }
+  }
+
+    statement {
+    sid = "113"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [ "${aws_iam_user.Mohamed.arn}" ]
+    }    
+
+    actions = [ 
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+      "s3:ListBucket"
+     ]
+
+     resources = [ 
+      aws_s3_bucket.s3_01.arn,
+      "${aws_s3_bucket.s3_01.arn}/*"
+      ]
+  }
 }
+
+# Create S3 objects (i.e directories)
+resource "aws_s3_object" "directory_object_s3_01_mostafa" {
+  bucket = aws_s3_bucket.s3_01.id
+  key = "mostafa/"
+  content_type = "application/x-directoy"
+}
+
+
+
+
+
+# s3api commands
+# aws s3api put-object --bucket eraki-s3-dev-01 --key mostafa/uploadMostafa.file --body uploadMostafa.file --grant-full-control id="b6d5c09c7a1d221375aced56c0bd9d32a1591e6a1512b61f1924e0e505c715cd" --profile mostafa
