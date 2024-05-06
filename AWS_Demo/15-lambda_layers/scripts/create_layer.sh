b7fb44d40ce9e36af2bc249adc3034d9ec33d5ff
Mohamed El-Eraki <mohamed.eleraki@enzagroup.global>
2024-05-06 11:09:56 +0300

    Adjust the layer creation method

--- AWS_Demo/15-lambda_layers/scripts/create_layer.sh
+++ AWS_Demo/15-lambda_layers/scripts/create_layer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create a Directory holds all function dependencies
-mkdir scripts/fetchVPCs_dependencies; cd scripts/fetchVPCs_dependencies
+mkdir scripts/fetchVPCs_dependencies; cd scripts/fetchVPCs_dependencies; mkdir python
 
 # Create a virtual environment with python3.11
 python3.11 -m venv fetchVPCs_depens_env
@@ -9,15 +9,19 @@ python3.11 -m venv fetchVPCs_depens_env
 # Activate the virtual environment
 source ./fetchVPCs_depens_env/bin/activate
 
-# Install boto3 library inside the virtual environment - install all the libraries here
+# Install boto3 library inside the virtual environment - install all the libraries here - Or use requirements.txt
 pip install boto3
-#pip install pg8000
+pip install pg8000
 
 # Deactivate the virtual environment
-deactivate
+#deactivate
 
 # Navigate to the site-packages directory within the virtual environment
-cd fetchVPCs_depens_env/lib/python3.11/site-packages
+#cd fetchVPCs_depens_env/lib/python3.11/site-packages
+#cd fetchVPCs_depens_env/lib
+cp -r fetchVPCs_depens_env/lib python/
 
 # Create a zip file named my_deployment_package.zip containing the current directory content
-zip -r ../../../../../fetchVPCs_depens_packages.zip .
+#zip -r ../../../../../fetchVPCs_depens_packages.zip .
+#zip -r ../../../fetchVPCs_dependencies/python/fetchVPCs_depens_packages.zip .
+zip -r ../fetchVPCs_depens_packages.zip python/
