5fa90721d5e14bade955aa891d594d184b0fe026
Mohamed El-Eraki <mohamed.eleraki@enzagroup.global>
2024-05-25 23:23:42 +0300

    update persentaions code

--- AWS_Demo/17-IaC-Terraform_Presentation/main.tf
+++ AWS_Demo/17-IaC-Terraform_Presentation/main.tf
@@ -1,21 +1,4 @@
 # Configure aws provider
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-    backend "s3" {
-    bucket = "erakiterrafromstatefiles"
-    key    = "iaC-Terraform_presentatino.tfstate"
-    region = "us-east-1"
-    profile = "eraki"
-    }
-}
-
-# Configure aws provider
 provider "aws" {
   region  = "us-east-1"
   profile = "eraki"
