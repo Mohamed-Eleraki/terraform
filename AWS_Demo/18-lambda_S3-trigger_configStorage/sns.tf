26e9c04bdcfe9de4c0d69e78311c1e4726bb8c37
Mohamed El-Eraki <mohamed.eleraki@enzagroup.global>
2024-06-06 13:09:39 +0300

    UPDATE DIAGREAM

--- AWS_Demo/18-lambda_S3-trigger_configStorage/sns.tf
+++ AWS_Demo/18-lambda_S3-trigger_configStorage/sns.tf
@@ -6,5 +6,5 @@ resource "aws_sns_topic" "migration_lambda_failures" {
 resource "aws_sns_topic_subscription" "sns_migration_lambda_email_subscription" {
   topic_arn = aws_sns_topic.migration_lambda_failures.arn
   protocol  = "email"
-  endpoint  = "mohamed-ibrahim2021@outlook.com"
+  endpoint  = "YOUR-EMAIL-ADDRESS"
 }
