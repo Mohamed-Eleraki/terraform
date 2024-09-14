d3d8f90bb765e98d5540ed2f090e3a694e2c4ed4
Mohamed El Eraki <mohamed-ibrahim2021@outlook.com>
2024-09-14 16:09:11 +0300

    craft test pipeline project

---
+++ AWS_Demo/37-build-test-pipeline/main.tf
@@ -0,0 +1,7 @@
+resource "aws_vpc" "vpc_01" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "vpc_01"
+  }
+}
---
+++ AWS_Demo/37-build-test-pipeline/terraform.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "erakiterrafromstatefiles"
+    key = "test-pipeline.tf"
+    region = "us-east-1"
+    #profile = "eraki"
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+  #profile = "eraki"
+}
